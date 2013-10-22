f5eb53cca547a03ed08f836d54bf8527a2dc8604
Shreyas <seekshreyas@gmail.com>
2013-10-21 17:42:13 -0700

    started workin on extracting the details

--- scraper.py
+++ scraper.py
@@ -4,12 +4,13 @@
 """
 Scraper
 ==========
-Given, the url of an andoid application, this module is meant to be able
+Given, the ANDROID-APP-URL, this module is meant to be able
 to extract an agreed upon set of features about the app
 - Name
 - Company
 - AppCategory
 - AppId
+- Price
 - Rating
     - [
         (OneStarRating, OneStarRatingCount),
@@ -36,27 +37,49 @@ python_version = "Python 2.7.5 :: Anaconda 1.6.1 (x86_64)"
 
 from __future__ import division
 from optparse import OptionParser
+from bs4 import BeautifulSoup
+from urllib import urlopen
 
 
 def getAppUrl():
     optionparser = OptionParser()
 
-    optionparser.add_option('-u', '--url', dest='app')
+    optionparser.add_option('-u', '--url', dest='appurl')
 
     (option, args) = optionparser.parse_args()
 
-    if not option.app:
+    if not option.appurl:
         return optionparser.error('data not provided.\n Usage: --url="path.to.appurl"')
 
-    return { 'url' : option.app }
+    return { 'url' : option.appurl }
+
+
+
+def getAppFeatures(app):
+    """
+    Given the Application URL, Extract the desired features
+    """
+    pageHtml = urlopen(app).read()
+
+    pageSoup = BeautifulSoup(pageHtml)
+
+    appId = app.split('?')[1]
+    appName = pageSoup.findAll('div', {"class":"document-title"})
+
+    appDetails = {}
+    appDetails['appId'] = appId
+    appDetails['appName'] = appName
+
+    return appDetails
 
 
 def main():
     # print __doc__
 
     appUrl = getAppUrl()
+    features = getAppFeatures(appUrl['url'])
 
-    print appUrl
+    print features
 
 
 if __name__ == '__main__':
