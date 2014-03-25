c6e88d27d85e210d8ce1d601def2f6a31010ef41
Shreyas <seekshreyas@gmail.com>
2014-03-25 15:28:53 -0700

    data values in dataframe working.. working on classifier

--- appClassifierBenchmark.py
+++ appClassifierBenchmark.py
@@ -16,6 +16,7 @@ from __future__ import division
 from optparse import OptionParser
 import pandas as pd
 import numpy as np
+from sklearn import svm
 
 def getUserInput():
     """
@@ -80,27 +81,48 @@ def prepareClassifier(df):
     Classify the apps
     """
 
+
     def classify(cDf):
         """
         Given the dataframe combined with equal fair and unfair apps,
         classify them
         """
+        cDf = cDf.reindex(np.random.permutation(cDf.index)) # shuffle the dataframe
+        featCols = set(cDf.columns)
+        featCols.remove('appLabel')
 
-        cDf = cDf.reindex(np.random.permutation(cDf.index))
+        X = cDf[list(featCols)]
+        Y = cDf['appLabel']
 
-        print cDf.head()
+
+        svc = svm.SVC() # initiating the SVM based classifier
+        Y_pred_svc = svc.fit(Y[:36], X).predict(Y[37:46]) # train on first 36 and test on last 10
+
+        print "SVM classifier Prediction Errors: \t\t%5d" % (Y_pred_svc != Y).sum()
+
+
+
+
 
     fairDf = df[df['appLabel'] == False]
     unfairDf = df[df['appLabel'] == True]
+
+    # print "fairDf", fairDf.values
+
+    # print "unfairDf", unfairDf.values
 
     # calculate total possible splits of fair data frame relatie to
     # size of unfair dataframe
     splits = len(fairDf) // len(unfairDf)
 
     for i in range(splits):
-        clDf = fairDf[i : i+len(unfairDf)] + unfairDf
+        clDf = fairDf[i : i+len(unfairDf)].append(unfairDf)
 
+        # print fairDf.values, unfairDf.values
+        print "Classifying %d th split of fair apps with unfair app" % (i)
+        print "-" * 79
         classify(clDf)
+        print "\n\n"
 
 
 
